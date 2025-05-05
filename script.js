f87f5c6445d5876f9137432ba90a2ee65040411d
edsooneto <168638099+edsooneto@users.noreply.github.com>
2025-05-05 18:33:25 -0300

    a

--- script.js
+++
@@ -1,6 +0,0 @@
-const shareBtn = document.getElementById("share-btn");
-const shareOptions = document.getElementById("share-options");
-
-shareBtn.addEventListener("click", () => {
-  shareOptions.classList.toggle("hidden");
-});
